0cb99e9c3417c706454e84a9f9973469fbd11d19
B4S1C-Coder <saksham2005mittal@gmail.com>
2025-11-25 00:07:54 +0530

    Added frontend client

--- src/server.js
+++ src/server.js
@@ -27,6 +27,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: 'http://localhost:3000', // or '*' for dev
+  credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
 }));
